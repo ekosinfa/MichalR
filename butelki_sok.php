e7b030088f1afa83fa43a3c431326c7321dffd82
michubest <michubest@gmail.com>
2014-09-29 21:31:12 +0200

    Create butelki_sok.php

---
+++ butelki_sok.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$i=99;
+while($i!=0){
+print $i ;
+print " butelek soku jabłkowego na ścianie, ";
+print $i;
+print "butelek soku. " . "\r\n";
+
+$i= $i - 1;
+
+print "Weż jedną i podaj dalej, ";
+print $i;
+print " butelek soku na ścianie.\r\n";}
+print "Nie ma więcej butelek soku na ścianie, nie ma więcej butelek soku."."\r\n".
+"Idź do sklepu i kup więcej, 99 butelek soku na scianie.";
