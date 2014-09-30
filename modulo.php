b240a70e7225297e399cee8dad59b1dedd6cfde3
michubest <michubest@gmail.com>
2014-09-30 17:52:20 +0200

    Create modulo.php

---
+++ modulo.php
@@ -0,0 +1,9 @@
+<?php
+for($i=1;$i!=101;$i++){
+if($i%3==0||$i%5==0){
+  if($i%3 ==0and $i%5==0){print "piec-trzy \r\n";}
+
+  if($i%3==0){ print "cy :3 \r\n";}
+  if($i%5==0){ print "pinc \r\n";}
+
+  }else{ print $i."\r\n";}}
